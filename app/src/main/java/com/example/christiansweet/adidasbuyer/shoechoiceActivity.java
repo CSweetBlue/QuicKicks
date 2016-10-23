c5c7f82ea700f74833e547cb3dee9c7b7f40bb24
Chris Ayoub <chris.ayoubtexas@gmail.com>
2016-10-23 04:49:16 -0500

    lots of changes, namely login and populating sizing data

--- app/src/main/java/com/example/christiansweet/adidasbuyer/shoechoiceActivity.java
+++ app/src/main/java/com/example/christiansweet/adidasbuyer/shoechoiceActivity.java
@@ -1,10 +1,8 @@
 package com.example.christiansweet.adidasbuyer;
 
 
-import android.app.LoaderManager;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
+
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,10 +10,8 @@ import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.View;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
@@ -26,22 +22,13 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.annotation.Annotation;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Arrays;
-
-import fi.iki.elonen.ServerRunner;
 
 /**
  * Created by Christian Sweet on 10/22/2016.
  */
 
-public class shoechoiceActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class shoechoiceActivity extends AppCompatActivity {
 
     private WebView myWebView;
     private LocalServer l;
@@ -60,6 +47,7 @@ public class shoechoiceActivity extends AppCompatActivity implements AdapterView
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shoechoice_size);
+
         myWebView = (WebView) findViewById(R.id.webview);
         myWebView.addJavascriptInterface(new MyJavaScriptInterface(), "HtmlViewer");
 
@@ -99,58 +87,51 @@ public class shoechoiceActivity extends AppCompatActivity implements AdapterView
         Spinner dropdown = (Spinner)findViewById(R.id.spinner);
         dropdown.setGravity(Gravity.CENTER_HORIZONTAL);
         String[] items = new String[]{"Adilette Slides", "Ultra Boost Uncaged Shoes", "Superstar Foundation Shoes", "NMD_C1 Trail Shoes"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        final String[] skus = new String[]{"280647", "BB4274", "C77124", "S81835"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
-//        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//                ((TextView) adapterView.getChildAt(0)).setGravity(Gravity.CENTER);
-//            }
-//        });
+
         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
-
-                if(pos == 0)
-                {
-                    String[] items1 = new String[]{"9", "10", "11", "12", "13", "14", "15"};
-                    Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-                    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(shoechoiceActivity.this, android.R.layout.simple_spinner_dropdown_item, items1);
-                    dropdown1.setAdapter(adapter1);
-                    ((TextView) adapterView.getChildAt(0)).setGravity(Gravity.CENTER);
+            public void onItemSelected(final AdapterView<?> adapterView, View view, final int pos, long l) {
 
 
-                    shoePicture.setImageResource(R.drawable.a280647);
+                    new AsyncTask<Void, Void, Void>() {
 
-                }
-                if(pos == 1)
-                {
-                    String[] items1 = new String[]{"9.5", "11", "12.5", "15"};
-                    Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-                    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(shoechoiceActivity.this, android.R.layout.simple_spinner_dropdown_item, items1);
-                    dropdown1.setAdapter(adapter1);
+                        String[] items;
 
+                        @Override
+                        protected Void doInBackground(Void... voids) {
+                            try {
+                                String[] data = Client.getSizes(skus[pos]).replaceAll("[{}]", "").split(", ");
+                                items = new String[data.length];
+                                for (int i = 0 ; i < data.length; i++) {
+                                    String[] vals = data[i].split("=");
+                                    items[i] = "Size: " + vals[0] + ", Stock Available: " + vals[1];
+                                }
 
-                    shoePicture.setImageResource(R.drawable.bb4274);
-                }
-                if(pos == 2)
-                {
-                    String[] items1 = new String[]{"4", "4.5", "5", "5.5", "6.5", "7", "7.5", "8", "8.5", "9", "9.5"};
-                    Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-                    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(shoechoiceActivity.this, android.R.layout.simple_spinner_dropdown_item, items1);
-                    dropdown1.setAdapter(adapter1);
+                            } catch (IOException e) {
+                                items = new String[]{};
+                            }
+                            return null;
+                        }
 
+                        @Override
+                        protected void onPostExecute(Void aVoid) {
+                            super.onPostExecute(aVoid);
+                            Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
+                            ArrayAdapter<String> adapter1 = new ArrayAdapter<>(shoechoiceActivity.this, android.R.layout.simple_spinner_dropdown_item, items);
+                            dropdown1.setAdapter(adapter1);
+                            ((TextView) adapterView.getChildAt(0)).setGravity(Gravity.CENTER);
 
-                    shoePicture.setImageResource(R.drawable.c77124);
-                }
-                if(pos == 3)
-                {
-                    String[] items1 = new String[]{"4", "4.5", "5", "5.5", "6.5", "7", "7.5", "8", "8.5", "9", "9.5"};
-                    Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-                    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(shoechoiceActivity.this, android.R.layout.simple_spinner_dropdown_item, items1);
-                    dropdown1.setAdapter(adapter1);
+                        }
+                    }.execute();
 
-                    shoePicture = (ImageView)findViewById(R.id.imageView);
-                    shoePicture.setImageResource(R.drawable.s81835);
+                switch (pos) {
+                    case 0: shoePicture.setImageResource(R.drawable.a280647); break;
+                    case 1: shoePicture.setImageResource(R.drawable.bb4274); break;
+                    case 2: shoePicture.setImageResource(R.drawable.c77124); break;
+                    case 3: shoePicture.setImageResource(R.drawable.s81835); break;
                 }
 
                 getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -168,7 +149,11 @@ public class shoechoiceActivity extends AppCompatActivity implements AdapterView
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
-                l = new LocalServer();
+                try {
+                    l = new LocalServer();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 try {
                     l.start();
                 } catch (IOException e) {
@@ -187,44 +172,9 @@ public class shoechoiceActivity extends AppCompatActivity implements AdapterView
 
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
-    {
-        if(pos == 0)
-        {
-            String[] items1 = new String[]{"9", "10", "11", "12", "13", "14", "15"};
-            Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items1);
-            dropdown1.setAdapter(adapter1);
-
-        }
-        if(pos == 1)
-        {
-            String[] items1 = new String[]{"9.5", "11", "12.5", "15"};
-            Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items1);
-            dropdown1.setAdapter(adapter1);
-        }
-        if(pos == 2)
-        {
-            String[] items1 = new String[]{"4", "4.5", "5", "5.5", "6.5", "7", "7.5", "8", "8.5", "9", "9.5"};
-            Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items1);
-            dropdown1.setAdapter(adapter1);
-        }
-
-    }
-
-    public void onNothingSelected(AdapterView<?> parent)
-    {
-        String[] items1 = new String[]{""};
-        Spinner dropdown1 = (Spinner)findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items1);
-        dropdown1.setAdapter(adapter1);
-    }
-
-
     @Override
     protected void onDestroy() {
+        super.onDestroy();
         l.stop();
     }
 
